13feb4d29a4af11fc0e2fb51407694da7373f48c
Mathieu Boulnois <mathieu.boulnois@hotmail.fr>
2024-11-12 11:54:45 +0100

    add completed boolean to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_12_104416) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_12_105222) do
   create_table "restaurants", force: :cascade do |t|
     t.string "title"
     t.text "details"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_12_104416) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
