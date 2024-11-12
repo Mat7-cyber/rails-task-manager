0c4d3292767754a82c051d3a754ee49152a880ee
Mathieu Boulnois <mathieu.boulnois@hotmail.fr>
2024-11-12 15:43:00 +0100

    exo fini

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_11_12_105222) do
-  create_table "restaurants", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
