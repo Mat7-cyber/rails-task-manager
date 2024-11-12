eae88df78b6e623b57b5f69783fc6e2c8cc99825
Mathieu Boulnois <mathieu.boulnois@hotmail.fr>
2024-11-12 11:42:26 +0100

    add task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_12_102350) do
-  create_table "tasks", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2024_11_12_104111) do
+  create_table "restaurants", force: :cascade do |t|
     t.string "title"
-    t.text "detail"
-    t.boolean "completed"
+    t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
